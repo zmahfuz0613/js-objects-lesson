9f0f5f09455e5dd668e31948fc0bb99e80442d44
Bruno Galvao <brunopgalvao@gmail.com>
2019-10-17 12:05:38 -0400

    Update car.js

--- car.js
+++ car.js
@@ -1,35 +1,65 @@
-const car = {
+const tesla1 = {
     make: 'Tesla',
     model: 'Model 3',
     awd: true,
-    color: 'matte black',
+    doors: 4,
     print: function() {
-        console.log(`${this.model}`)
+        console.log(`The make is ${this.make}, the model is ${this.model} is it awd? ${this.awd}, number of doors:  ${this.doors}`)
     }
 }
 
-const myTesla =  Object.create(car)
-myTesla.print()
+const tesla2 = {
+    make: 'Tesla',
+    model: 'Model X',
+    awd: true,
+    doors: 2,
+    print: function() {
+        console.log(`The make is ${this.make}, the model is ${this.model} is it awd? ${this.awd}, number of doors:  ${this.doors}`)
+    }
+}
 
-const Car = function(make, model, awd, color) {
+const tesla3 = {
+    make: 'Tesla',
+    model: 'Model S 90P',
+    awd: true,
+    doors: 4,
+    print: function() {
+        console.log(`The make is ${this.make}, the model is ${this.model} is it awd? ${this.awd}, number of doors:  ${this.doors}`)
+    }
+}
+
+// constructor function
+const Tesla = function(make, model, awd, doors) {
     this.make = make,
     this.model = model,
     this.awd = awd,
-    this.color = color,
-    this.print = function() {
-        console.log(this.model)
+    this.doors = doors,
+    this.print = function () {
+        console.log(`The make is ${this.make}, the model is ${this.model} is it awd? ${this.awd}, number of doors:  ${this.doors}`)
     }
 }
 
-const car1 = new Car('tesla', 'model x', true, 'grey')
-car1.print()
+const philipsTesla = new Tesla('Tesla', 'Model S', true, 4)
+const yevsTesla = new Tesla('Tesla', 'Model S 90PD', true, 4)
+const andresTesla = new Tesla('Tesla', 'Model S 90PD', true, 4)
 
-const printAll = function(car) {
-    for (let key in car) {
-        console.log(`${key} = ${car[key]}`)
+const printAll = function(tesla) {
+    for(let key in tesla) {
+        console.log(`${key} = ${tesla[key]}`)
     }
 }
 
-// printAll(car)
-console.log(Object.keys(car))
-console.log(Object.getOwnPropertyNames(car))
+// printAll(tesla1)
+
+// console.log(Object.keys(andresTesla))
+
+// immutable property
+const tesla = {}
+Object.defineProperty( tesla, "make", {
+  value: "Tesla",
+  writable: false,
+  enumerable: true,
+  configurable: true
+})
+
+console.log(Object.keys(tesla))
