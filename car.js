db77b0abb7a2ce976b6ee8ec5eb232a33b4cd143
Bruno Galvao <brunopgalvao@gmail.com>
2019-10-17 10:22:51 -0400

    Create car.js

---
+++ car.js
@@ -0,0 +1,25 @@
+const car = {
+    make: 'Tesla',
+    model: 'Model 3',
+    awd: true,
+    color: 'matte black',
+    print: function() {
+        console.log(`${this.model}`)
+    }
+}
+
+const myTesla =  Object.create(car)
+myTesla.print()
+
+const Car = function(make, model, awd, color) {
+    this.make = make,
+    this.model = model,
+    this.awd = awd,
+    this.color = color,
+    this.print = function() {
+        console.log(this.model)
+    }
+}
+
+const car1 = new Car('tesla', 'model x', true, 'grey')
+car1.print()
